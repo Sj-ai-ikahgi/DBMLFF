5d75c23ed0e87a3093c6e9351c64e03c3b34a533
Sj-ai-ikahgi <2412779523@qq.com>
2025-12-16 17:59:14 +0800

    Upload a new folder : 15tte_10ea_5lifsi

---
+++ 15tte_10ea_5lifsi/dbmlff_md_500K/run_mpich.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#SBATCH --partition=1080ti
+#SBATCH --job-name=md_test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+
+
+UDA_HOME="/share/app/cuda/cuda-11.3"
+export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
+export CPATH="${CUDA_HOME}/include:${CPATH}"
+export PATH="${CUDA_HOME}/bin:${PATH}"
+
+#nvhpc
+export NVARCH="$(uname -s)_$(uname -m)"
+export NVCOMPILERS="/share/app/nvhpc22.2"
+export MANPATH="${NVCOMPILERS}/${NVARCH}/22.2/compilers/man:${MANPATH}"
+export PATH="${NVCOMPILERS}/${NVARCH}/22.2/compilers/bin:${PATH}"
+
+#mkl
+. /share/app/intel2020u4/parallel_studio_xe_2020/psxevars.sh
+
+#mpich
+MPICH_HOME=/data/home/xiongrui1/sabaisheng/Shenjie/fitting/FSI/mpich-3.4.3
+export LD_LIBRARY_PATH="${MPICH_HOME}/lib:${LD_LIBRARY_PATH}"
+export CPATH="${MPICH_HOME}/include:${CPATH}"
+export PATH="${MPICH_HOME}/bin:${PATH}"
+
+#nccl
+NCCL_HOME="/share/app/nvhpc22.2/Linux_x86_64/22.2/comm_libs/nccl"
+export LD_LIBRARY_PATH="${NCCL_HOME}/lib:${LD_LIBRARY_PATH}"
+
+
+codePath=/data/home/xiongrui1/sabaisheng/Shenjie/fitting/FSI/dataset/md_src_ppp0
+
+rm -f OUT.* main_MD.x_*
+
+  mpirun -np ${SLURM_NPROCS} ${codePath}/main_MD.x >> main_MD.x_out 2>&1 >> main_MD.x_err
+
+exit 0
+
+
